0730e83c45ec7d6d82f69240ce88320bb98d9cb1
Bxnni78187 <benni.gbs@gmx.de>
2024-12-10 23:02:21 +0100

    add loggin

--- deamon/src/lib.rs
+++ deamon/src/lib.rs
@@ -1,10 +1,23 @@
+mod utils;
+
 use active_win_pos_rs::{get_active_window, ActiveWindow};
-use std::collections::HashMap;
 #[allow(unused)]
 use std::fs::File;
 #[allow(unused)]
 use std::io::Write;
+use std::{collections::HashMap, default};
 use tokio::time::{self, Duration};
+use utils::Second;
+
+impl Second for u64 {
+    fn seconds_to_hms(&self) -> (u64, u64, u64) {
+        let hours = self / 3600;
+        let minutes = (self % 3600) / 60;
+        let seconds = self % 60;
+        (hours, minutes, seconds)
+    }
+}
+
 pub async fn start_deamon() -> () {
     // Initialisiere den Daemon
     println!("Daemon gestartet...");
@@ -14,20 +27,31 @@ pub async fn start_deamon() -> () {
 
     // Prozessdaten speichern
     let mut app_usage: HashMap<String, u64> = HashMap::new();
+    let mut sec_past: u32 = 0;
     loop {
+        sec_past += 1;
+        println!("Seconds past: {sec_past}");
+
         interval.tick().await;
         let active_window = get_active_window();
         if active_window == Err(()) {
             continue;
         }
+
         let current_window_name = active_window.unwrap().app_name;
         *app_usage.entry(current_window_name).or_insert(0) += 1;
 
         println!("{:?}", app_usage)
     }
 }
-fn log_data() {
-    unimplemented!()
+fn log_data(app_usage: HashMap<String, u64>) {
+    let app_usage = app_usage
+        .values()
+        .map(|sec| {
+            let secs = sec.seconds_to_hms();
+            format!("{}.{}.{}", secs.0, secs.1, secs.2)
+        })
+        .collect::<HashMap<String, String>>();
 }
 
 #[cfg(test)]
---
+++ deamon/src/utils/mod.rs
@@ -0,0 +1,3 @@
+pub trait Second {
+    fn seconds_to_hms(&self) -> (u64, u64, u64);
+}
